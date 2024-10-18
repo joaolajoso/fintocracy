317f778fb089f078b16413dae7fe50cf6b71e29d
João Lajoso <54990286+joaolajoso@users.noreply.github.com>
2024-10-18 02:25:46 +0100

    Update fintocracy.py

--- fintocracy.py
+++ fintocracy.py
@@ -233,8 +233,8 @@ def generate_questions():
         "Generate multiple-choice questions about financial literacy, "
         "with the following content as a reference: \n\n"
         f"{markdown_content}\n"
-        "Please provide 10 questions along with their options and the correct answers, language is Portuguese of Portugal. On the correct answer add '>' at beggining no space "
-        "formatted as: '1. Question | Option1, Option2, Option3, Option4'."
+        "Please provide 10 questions along with their options and the correct answers, language is Portuguese of Portugal. On the correct answer add '>' at beggining with no space. theres is no special charecters."
+        "formatted as: 'Question | Option1, Option2, Option3, Option4'."
     )
     #and < at end. 
     try:
