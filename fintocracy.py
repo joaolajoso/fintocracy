38f9f852abfdf1576375ef86cff19526a8014621
João Lajoso <54990286+joaolajoso@users.noreply.github.com>
2024-10-18 11:52:07 +0100

    Update fintocracy.py

--- fintocracy.py
+++ fintocracy.py
@@ -319,7 +319,7 @@ def quiz_interativo_with_groq(nivel_atual):
             if respostas[i] == respostas_corretas[i]:
                 st.session_state['pontuacao'] += 1
 
-    print(respostas)
+        print(respostas)
     # Exibir pontuação final e atualizar o nível
     if st.button("Submeter Respostas"):
         # Coletar respostas do usuário
